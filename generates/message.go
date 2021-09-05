a2ac6507c73be265415fb5a1d816362c0f789d1b
Ivan <ivan.besevic_fr@yahoo.com>
2021-09-05 15:10:14 +0200

    improve code

--- generates/message.go
+++ generates/message.go
@@ -372,8 +372,6 @@ func serializer(i interface{}, firstLetter string, variableName string) {
 }
 
 func GenerateMessage(name string, packetId uint32) {
-	name = strings.ToLower(name[:1]) + name[1:]
-
 	for interfaceType := dispatchSerializer(); interfaceType != nil; interfaceType = dispatchSerializer() {
 		name_ := scan("Enter variable name:", true)
 		serializer(interfaceType, (name)[:2], name_)
